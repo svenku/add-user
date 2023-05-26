565d1e61665ec74a3ee49b2228fe6da8e444f045
svenku <sven.kunsing@gmail.com>
2023-05-26 20:01:20 +0300

    Add UserList component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 function App() {
   return (
     <div>
-      <AddUser/>
+      <AddUser />
+      <UserList users={[]}/>
     </div>
   );
 }
