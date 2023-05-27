b460ea5523611b873565ad4498b7bb5e9d2b4431
svenku <sven.kunsing@gmail.com>
2023-05-27 12:43:01 +0300

    Manage list of users through state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UserList";
 
 function App() {
+
+const [userList, setUserList] = useState([]);
+
+const addUserHandler = (uName, uAge) => {
+  setUserList((prevUserList) => {
+    return [...prevUserList, {name: uName, age: uAge, id: uuidv4()}];
+  });
+};
+
   return (
     <div>
-      <AddUser />
-      <UserList users={[]}/>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList}/>
     </div>
   );
 }
