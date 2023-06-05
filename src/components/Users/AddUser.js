ec92526278e579849a33a3a724ec1976a430c8da
svenku <sven.kunsing@gmail.com>
2023-06-05 14:02:53 +0300

    Add error handling and handle clicks for error modal.

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -8,15 +8,24 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState("");
 
   const addUserHandler = (event) => {
     event.preventDefault();
 
-    if (
-      enteredUsername.trim().length === 0 ||
-      enteredAge.trim().length === 0 ||
-      +enteredAge < 1
-    ) {
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Values cannot be empty",
+      });
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      setError({
+        title: "Invalid input",
+        message: "Age can not be negative",
+      });
       return;
     }
 
@@ -33,9 +42,19 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal title="Error" message="Something went wrong." />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
